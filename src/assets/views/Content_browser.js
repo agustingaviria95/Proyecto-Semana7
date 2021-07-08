d04bd5dd46ba38da97fd95a216a52de6854cfaa2
Leidy Muñoz <leidy.munoz@alteru.co>
2021-07-08 18:00:09 -0500

    Arreglos en estilos

--- src/assets/views/Content_browser.js
+++ src/assets/views/Content_browser.js
@@ -1,51 +1,38 @@
 import img_logo from "../img/Logo-header.png";
-import img_arrow from "../img/Arrow-downicon.png";
 import img_close from "../img/close-icon.png";
 import img_search from "../img/Search-icon.png";
 
 const Content_browser = () => {
     const view =`
-    <section class="Content_browser"> 
-        <section class="title-home-section">
+        <section class="Content_browser">
         <div class="title-home-div">
             <span><img src="${img_logo}" alt="Logo Sci-fi Site"></span>
-            <span> <p>Buscador <br> de relatorías</p> </span>
+            <span> <p>Buscador <br> de entretenimiento</p> </span>
         </div>
         </section>
-        <section class="browser-home">
-        <section class="search-section-home">
-            <div class="search-div-home">
-                <span class="search-span-home">
-                    <span class="search-input-span">
-                        <span class="search-icon">
-                            <img src="${img_search}" alt="Icono de buscar">
-                        </span>
-                        <input type="text" placeholder="¿Qué buscas?">
-                    </span>
-                    <span class="search-filter-span">
-                        <span><a href="">Ver filtros</a></span>
-                        <span class="arrow-icon">
-                            <img src="${img_arrow}" alt="Icono despleglable">
-                        </span>
-                    </span>
-                </span>
+        <nav class="navbar navbar-light bg-transparent">
+            <div class="container-fluid">
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="¿Qué buscas?" aria-label="Search">
+                    <span><img src="${img_search}"</span>
+                    <button class="btn btn-outline-primary" type="submit">Buscar</button>
+                </form>
             </div>
-            <span class="search-button-home"><button>Buscar</button></span>
-        </section>
-        <section id="filter-home">
-            <section class="filter-section-container" >
-                <div class="filter-section-1">
-                    <p>Videojuegos</p><span><img src="${img_close}" alt="Videojuegos"></span>
-                </div>
-                <div class="filter-section">
-                    <p>Películas</p><span><img src="${img_close}" alt="Películas"></span>
-                </div>
-                <div class="filter-section">
-                    <p>Series</p><span><img src="${img_close}" alt="Series"></span>
-                </div>
-            </section> 
-        </section>
-    </section>
+        </nav>
+        <div class="filter-container" >
+            <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+            Filtrar por videojuegos <span><img src="${img_close}"</span>
+            </button>
+            <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Right popover">
+            Filtrar por Películas <span><img src="${img_close}"</span>
+            </button>
+            <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Bottom popover">
+            Filtrar por Series <span><img src="${img_close}"</span>
+            </button>
+            <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="left" data-bs-content="Left popover">
+            Eliminar filtros <span><img src="${img_close}"</span>
+            </button>
+        </div>
         `;
     return view;
 };
