db5c9c64afb2ecced457f8c077593861d3e58530
Mao-Ocampo <84541905+Mao-Ocampo@users.noreply.github.com>
2021-07-09 12:39:50 -0500

    Agregador de la paginación inferior

--- src/views/Results/Results.js
+++ src/views/Results/Results.js
@@ -8,21 +8,21 @@ const Results = () => {
     const view = `
     <section class="row">
       <div class="col-sm-12 col-lg-8">
-        <h3 class="text-white text-center title-games-section">Los Mejores Videojuegos</h3>
+        <h3 class="text-black text-center title-games-section">Los Mejores Videojuegos</h3>
           <div class="card-group">
-            <div class="card bg-transparent games-container">
-              <a href="/"><img src="${game1}" class="card-img-top" alt="Halo La Colección del Jefe Maestro"></a>
+            <div class="card bg-transparent border-light games-container">
+              <a href="/"><img src="${game1}" class="card-img-top" alt="Halo MC Collection"></a>
             <div class="card-body">
-              <h5 class="card-title text-white text-center">Halo La Colección del Jefe Maestro</h5>
-              <p class="text-white text-center">Disfruta con la colección completa totalmente remasterizada para PC y consolas XBOX, incluye Halo Combat Evolved Anniversary, Halo 2 Anniversary, Halo 3, Halo 3 ODST, Halo 4 y Halo Reach.</p>
+              <h5 class="card-title text-black text-center">Halo MC Collection</h5><hr>
+              <p class="text-black text-center">Disfruta con la colección completa totalmente remasterizada para PC y consolas XBOX, incluye Halo Combat Evolved Anniversary, Halo 2 Anniversary, Halo 3, Halo 3 ODST, Halo 4 y Halo Reach.</p>
             </div>
             <button type="button" class="btn btn-primary btn-lg btn-block">Juega Ahora</button>
           </div>
-          <div class="card bg-transparent games-container">
+          <div class="card bg-transparent border-light games-container">
             <a href="/"><img src="${game2}" class="card-img-top" alt="Final Fantasy VII Remake"></a>
             <div class="card-body">
-              <h5 class="card-title text-white text-center">Final Fantasy VII Remake</h5><hr>
-              <p class="text-white text-center">Versión remasterizada del popular juego de rol de Square Enix publicado en 1997 para la primera PlayStation, combina la acción en tiempo real similar a Dissidia Final Fantasy con otros elementos estratégicos.</p>
+              <h5 class="card-title text-black text-center">Final Fantasy VII Remake</h5><hr>
+              <p class="text-black text-center">Versión remasterizada del popular juego de rol de Square Enix publicado en 1997 para la primera PlayStation, combina la acción en tiempo real similar a Dissidia Final Fantasy con otros elementos estratégicos.</p>
             </div>
             <button type="button" class="btn btn-primary btn-lg btn-block">Juega Ahora</button>
           </div>
@@ -31,23 +31,34 @@ const Results = () => {
     <section class="row">
       <div class="col-sm-12 col-lg-8">
         <div class="card-group">
-            <div class="card bg-transparent games-container">
+            <div class="card bg-transparent border-light games-container">
             <a href="/"><img src="${game3}" class="card-img-top" alt="Horizon Zero Dawn"></a>
             <div class="card-body">
-              <h5 class="card-title text-white text-center">Horizon Zero Dawn</h5><hr>
-              <p class="text-white text-center">Conviértete en la habilidosa cazadora Aloy y explora un mundo exuberante habitado por misteriosas criaturas mecanizadas, en un nuevo y emocionante juego de rol, acción y un impresionante mundo abierto.</p>
+              <h5 class="card-title text-black text-center">Horizon Zero Dawn</h5><hr>
+              <p class="text-black text-center">Conviértete en la habilidosa cazadora Aloy y explora un mundo exuberante habitado por misteriosas criaturas mecanizadas, en un nuevo y emocionante juego de rol, acción y un impresionante mundo abierto.</p>
             </div>
             <button type="button" class="btn btn-primary btn-lg btn-block">Juega Ahora</button>
           </div>
-          <div class="card bg-transparent games-container">
+          <div class="card bg-transparent border-light games-container">
             <a href="/"><img src="${game4}" class="card-img-top" alt="Resident Evil Village"></a>
             <div class="card-body">
-              <h5 class="card-title text-white text-center">Resident Evil Village</h5><hr>
-              <p class="text-white text-center">Llega la nueva generación de "survival horror" con Resident Evil Village, la octava entrega de su historia principal. Disfruta de sus gráficos ultrarrealistas y lucha por sobrevivir contra todos los peligros letales.</p>
+              <h5 class="card-title text-black text-center">Resident Evil Village</h5><hr>
+              <p class="text-black text-center">Llega la nueva generación de "survival horror" con Resident Evil Village, la octava entrega de su historia principal. Disfruta de sus gráficos ultrarrealistas y lucha por sobrevivir contra todos los peligros letales.</p>
             </div>
             <button type="button" class="btn btn-primary btn-lg btn-block">Juega Ahora</button>
           </div>
         </div>
+    </section>
+    <section class="nav-pages">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">Siguiente</a></li>
+        </ul>
+      </nav>
     </section>
     `;
     return view;
