5a7ae088358276ceb7d2c356be7d9d2a377986db
Mao-Ocampo <84541905+Mao-Ocampo@users.noreply.github.com>
2021-07-10 16:15:02 -0500

    Preparación antes del Merge

--- App.js
+++
@@ -1,7 +0,0 @@
-import "./assets/styles.css";
-import router from "./routes";
-
-window.addEventListener("load", router(window.location.hash));
-window.addEventListener("hashchange", () => {
-  router(window.location.hash);
-});
